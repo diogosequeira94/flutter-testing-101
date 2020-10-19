b4839324d39647674f26d85e997987beeb1581a7
DiogoSequeira <Diogo.Sequeira@ctw.bmwgroup.com>
2020-10-19 16:56:29 +0100

    added Calculator class, first Unit Test

---
+++ lib/calculator.dart
@@ -0,0 +1,9 @@
+class Calculator {
+  double add(double a, double b) => a+b;
+
+  double sub(double a, double b) => a-b;
+
+  double multiply(double a, double b) => a*b;
+
+  double divide(double a, double b) => a/b;
+}
